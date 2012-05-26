8b4195a6e3abbea07019153ef44264cda0615f8a
Eric Guo <eric.guo@sandisk.com>
2012-05-26 16:12:30 +0800

    Welcome to Bootstrap Project Startup Site
    *. using Sass template
    *. using ruby.taobao.org in Gemfile
    *. add win32console for more beatify out in Windows
    *. config.assets.debug = false in config/environments/development.rb to focus function at first
    *. Add developer document reference

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlForm::Application.config.secret_token = 'df8012930b3f34ff34cc8157fae77b710cb9acd7d061e29ce5e523fed4d24b7e9df0d71e00dd633db1db307aeb2e31807c2c888865152fd0bbff4f1ba590bf1d'

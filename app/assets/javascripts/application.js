87c97e450f321796d9d1a7acc525f056231a9567
Eric Guo <eric.guo@sandisk.com>
2013-10-02 17:05:31 +0800

    upgrade to the rails 4 default setting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require turbolinks
 //= require twitter/bootstrap
 //= require_tree .
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PlForm::Application.config.secret_token = 'df8012930b3f34ff34cc8157fae77b710cb9acd7d061e29ce5e523fed4d24b7e9df0d71e00dd633db1db307aeb2e31807c2c888865152fd0bbff4f1ba590bf1d'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlForm::Application.config.secret_key_base = '4f34893f2cc80549975650b9ef98effa9e098857b45a79bbb705bf433a9d1aa754e18e707cf9b185c03eaa80dc32839df2b29cbb4ca8bf293a96816b6ba32f3d'

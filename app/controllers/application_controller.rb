c269850d8a32266d50ecf150b5aaf6050e4d6664
Eric Guo <eric.guo@sandisk.com>
2014-02-24 12:51:09 +0800

    after running `rake db:migrate` and `rails generate devise_ldap_authenticatable:install`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+  rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
+    render :text => exception, :status => 500
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-end
+
+
+nd

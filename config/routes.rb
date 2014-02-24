d32529d55c1ea63d79161b2b7d6ce787dfb3bb58
Eric Guo <eric.guo@sandisk.com>
2014-02-24 12:42:27 +0800

    after running `rails generate devise User` and `rails g devise:views`

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PlForm::Application.routes.draw do
+  devise_for :users
   get "home/index"
   root :to => 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.

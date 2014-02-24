4b8f6f87f0152d4f926511323e8ed95029978208
Eric Guo <eric.guo@sandisk.com>
2014-02-24 22:26:34 +0800

    fix can not remember me by add token field
    via: rails g migration add_remember_token_to_users remember_token:string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224130623) do
+ActiveRecord::Schema.define(version: 20140224142437) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140224130623) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "badge"
+    t.string   "remember_token"
   end
 
   add_index "users", ["badge"], name: "index_users_on_badge", unique: true

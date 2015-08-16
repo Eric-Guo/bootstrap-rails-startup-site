38534b5e1a9589b7389fb84321142b296fa48b5c
Eric Guo <eric.guo@sandisk.com>
2015-08-16 13:19:13 +0800

    Fix missing signed_in_user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def signed_in_user
+    store_location
+    redirect_to new_user_session_path, notice: 'Please sign in.' unless signed_in?
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath if request.get?
+  end
 end

3b17958e9b2f52adb29656bcc56392bf3237ece2
Eric Guo <eric.guo@sandisk.com>
2015-08-16 13:30:14 +0800

    should not generate scaffolds

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,14 @@ Bundler.require(:default, Rails.env)
 
 module PlForm
   class Application < Rails::Application
+    config.generators do |g|
+        g.test_framework :rspec
+        g.integration_tool :rspec
+        g.stylesheets false
+        g.javascripts false
+        g.helper false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

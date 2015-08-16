3e8921ab76a3b31523c3e12b682182baa08ebdc4
Eric Guo <eric.guo@sandisk.com>
2015-08-16 14:50:41 +0800

    optimize the DB performance

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -50,7 +50,7 @@ class ProjectsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = Project.find(params[:id])
+      @project = Project.includes(cases: :tester_user, suites: {cases: :tester_user}).find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
